3d401614b82e5b4e25b352ad9e814d9e6b0e8f1f
Wichanan <wichananm65@nu.ac.th>
2025-06-20 20:39:52 +0700

    add grid view to menu page

--- lib/Page/menu_page.dart
+++ lib/Page/menu_page.dart
@@ -10,11 +10,11 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Healthy Plan',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
       ),
-      home: const MyMenuPage(title: 'Flutter Demo Home Page'),
+      home: const MyMenuPage(title: 'เมนู'),
     );
   }
 }
@@ -29,8 +29,6 @@ class MyMenuPage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyMenuPage> {
-  String selectedPage = '';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,18 +81,14 @@ class _MyHomePageState extends State<MyMenuPage> {
               leading: const Icon(Icons.account_circle),
               title: const Text('โปรไฟล์'),
               onTap: () {
-                setState(() {
-                  selectedPage = 'Profile';
-                });
+                setState(() {});
               },
             ),
             ListTile(
               leading: const Icon(Icons.message),
               title: const Text('เมนู'),
               onTap: () {
-                setState(() {
-                  selectedPage = 'Menu';
-                });
+                setState(() {});
               },
             ),
 
@@ -102,9 +96,7 @@ class _MyHomePageState extends State<MyMenuPage> {
               leading: const Icon(Icons.settings),
               title: const Text('Settings'),
               onTap: () {
-                setState(() {
-                  selectedPage = 'Settings';
-                });
+                setState(() {});
               },
             ),
           ],
@@ -112,8 +104,56 @@ class _MyHomePageState extends State<MyMenuPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text('Page: $selectedPage')],
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'ค้นหา...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 20,
+                  ),
+                ),
+                onChanged: (value) {
+                  print('Search: $value');
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    childAspectRatio: 1.5,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 12),
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.green),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
